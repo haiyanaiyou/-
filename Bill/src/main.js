6438bf1bcde32a5f9f4d8b94a5b19cbd9d4d3039
=DingPujiang <=dingpujiang@gujinsuo.com.cn>
2017-08-07 14:29:49 +0800

    commit

--- Bill/src/main.js
+++ Bill/src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store/index.js'
+import './assets/lib/Chart.js'
+import './assets/lib/flexible.js'
+/* 使用vux插件的提示功能 */
+import { ToastPlugin } from 'vux'
+Vue.use(ToastPlugin)
 
 Vue.config.productionTip = false
 
@@ -10,6 +16,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
--- Bill/src/router/index.js
+++ Bill/src/router/index.js
@@ -1,15 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Guide from '../views/guide/Guide.vue'
+import Home from '../views/home/Home.vue'
+
+import Tool from '../assets/lib/Tool'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
+const router = new Router({
+  routes:[
+    /* 引导页 */
+    {
+      path: '/guide',
+      name: 'guide',
+      component: Guide,
+      meta: {
+        status: 0
+      }
+
+    },
+    /* 首页 */
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'home',
+      component: Home,
+      meta: {
+        status: 1
+      }
+
     }
   ]
-})
+});
+
+/* 判断用户是否是第一次打开App页面 根据具体情况判断是否显示引导页面
+  *设置路由之间的跳转动画
+
+*/
+router.beforeEach((to, from, next) => {
+  let store = this.a.app.$store;
+  if (store) {
+    if (from.meta.status > to.meta.status) store.commit('SET_ANIMATE_NAME', 'vux-pop-out');
+    else store.commit('SET_ANIMATE_NAME', 'vux-pop-in');
+  }
+    let is_not_first = Tool.dataToLocalStorageOperate.achieve('is_not_first');
+    if ( !is_not_first && to.path != '/guide' ) next ('/guide');
+    else if ( is_not_first && to.path == '/guide' ) next('/');
+    else next();
+
+});
+
+export default router
